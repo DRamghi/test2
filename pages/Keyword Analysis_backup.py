3eeb93865778c84db933809ef26f8e0626a15d81
DRamghi <114323579+DRamghi@users.noreply.github.com>
2023-07-25 23:18:02 +0900

    Add files via upload

--- pages/Keyword Analysis_backup.py
+++ pages/Keyword Analysis_backup.py
@@ -186,28 +186,41 @@ if search_word and start_date and end_date != "0":
     date_data = data[data['Date'].between(start_date, end_date)]  #####대상기간 내 기사 본문 추출
 
 
-    # 특정 키워드 기사 index 추출(제목에 포함 or 본문에 1번 이상 포함)
-    full_date_list = pd.date_range(start=start_date, end=end_date)
+    # 특정 키워드 기사 index 추출(제목에 포함 or 본문에 2번 이상 포함)
+    target_index_list = []
+    for i in range(0, len(date_data)):
+        title_i = date_data.iloc[i, 2]
+        body_i = date_data.iloc[i, 4]
+        if title_i.count(search_word) > 0 or body_i.count(search_word) > 1:
+            target_index_list.append(i)
 
-    count_list = []
-    index_list = []
-    date_list = []
+    result_list = date_data.iloc[target_index_list]
+    result_list
+
+    full_date_list = pd.date_range(start=start_date, end=end_date)
+    date_list = set(result_list['Date'])
+    date_list = list(date_list)
+    date_list.sort(reverse=False)
+
+    target_date_list = []
+    count = []
     for i in full_date_list:
-        a = date_data[date_data['Date'] == i]
-        count = 0
-        for j in range(0, len(a)):
-            title_j = a.iloc[j, 2]
-            body_j = a.iloc[j, 4]
-            if title_j.count(search_word) > 0 or body_j.count(search_word) > 0:
-                count = count + title_j.count(search_word) + body_j.count(search_word)
-                index_list.append(a.index[j])
-                if date_list.count(i) == 0:
-                    date_list.append(i)
-        count_list.append(count)
-    d = pd.DataFrame([full_date_list, count_list])
+        if i in date_list:
+            a = result_list[result_list['Date'] == i]  # 특정 일자의 기사 불러오기
+            b = a['Title'].tolist()
+            b = ' '.join(b)
+            count.append(b.count(search_word))
+            target_date_list.append(i)
+        else:
+            count.append(0)
+            target_date_list.append(i)
+
+    d = pd.DataFrame([target_date_list, count])
+
     d = d.transpose()  # 행 열 전환
     d.rename(columns={0: "date"}, inplace=True)
     d.rename(columns={1: "count"}, inplace=True)
+
 
     fig1 = px.line(d, x="date", y="count")
     fig1.update_layout(margin=dict(l=0, r=0, t=0, b=0, pad=0))
@@ -218,6 +231,7 @@ if search_word and start_date and end_date != "0":
     st.markdown("""---""")
 
 
+
     ##########불용어 사전#########################
     stop_words = "이번 담당 여러분 관련 이날 이후 오후 오전 경우 기간 때문 관계자 최근 기준 설명 연합뉴스 예정 증가 가운데 상당 가량 추진 아마 대략 방침 현지시간"
     stop_words = set(stop_words.split(' '))
@@ -225,9 +239,9 @@ if search_word and start_date and end_date != "0":
 
     #########특정 키워드 연관어 분석################
 
-    result_list = date_data.loc[index_list]
+    date_list = set(result_list['Date'])
+    date_list = list(date_list)
     date_list.sort(reverse=False)
-
     period = end_date - start_date + timedelta(days=1)
     period = period.days
 
@@ -248,48 +262,6 @@ if search_word and start_date and end_date != "0":
             date_result = result_list[
                 result_list['Date'].between(start_day, end_day)]
 
-            if len(date_result) == 0:
-                pass
-            else:
-                text = '\n'.join(date_result['Body'])
-                lines = text.split(".")
-
-                # 명사 추출
-                words_list = []
-                for line in lines:
-                    if search_word in line:
-                        nouns = mecab.nouns(line)
-                        for noun in nouns:
-                            if len(noun) > 1 and noun not in stop_words and noun != search_word:
-                                words_list.append(noun)  # 한 문장에서 명사들만 추출(일자별)
-                                total_word_list.append(noun)  # 한 문장에서 명사들만 추출(전 기간)
-
-                c = Counter(words_list)
-
-                top_related_words = dict(c.most_common(10))
-                if str_start_day == str_end_day:
-                    first_column = str_end_day
-                else: first_column = f'{str_start_day}~{str_end_day}'
-
-                date_top_related_words = pd.DataFrame(list(top_related_words.items()), columns=[first_column, "second"])
-                for d in range(0,10):
-                    date_top_related_words.iloc[d,0] = f'{date_top_related_words.iloc[d,0]}  :  {date_top_related_words.iloc[d,1]}건'
-                date_top_related_words.drop(labels='second', axis=1, inplace=True)
-                date_top_related_words.index = date_top_related_words.index + 1
-                keywords_table = pd.concat([keywords_table, date_top_related_words], axis=1, join='outer')
-
-    if period % n == 0:
-        pass
-    else:
-        plus_day = start_date + timedelta(days=(n * (i + 1)))
-        str_plus_day = str(plus_day)[5:10].replace('-', '.') + " ~"
-        plus_alpha_day = start_date + timedelta(days=(n * (i + 1)) + period % n -1)
-        str_plus_alpha_day = str(plus_alpha_day)[5:10].replace('-', '.')
-        date_result = result_list[result_list['Date'].between(plus_day, plus_alpha_day)]
-
-        if len(date_result) == 0:
-            pass
-        else:
             text = '\n'.join(date_result['Body'])
             lines = text.split(".")
 
@@ -306,19 +278,66 @@ if search_word and start_date and end_date != "0":
             c = Counter(words_list)
 
             top_related_words = dict(c.most_common(10))
+            if str_start_day == str_end_day:
+                first_column = str_end_day
+            else: first_column = f'{str_start_day}~{str_end_day}'
 
-            if str_plus_day == str_plus_alpha_day:
-                last_column = str_plus_alpha_day
-            else:
-                last_column = f'{str_plus_day}~{str_plus_alpha_day}'
-
-            date_top_related_words = pd.DataFrame(list(top_related_words.items()), columns=[last_column, "second"])
-
-            for i in range(0, 10):
-                date_top_related_words.iloc[i, 0] = f'{date_top_related_words.iloc[i, 0]}  :  {date_top_related_words.iloc[i, 1]}건'
+            date_top_related_words = pd.DataFrame(list(top_related_words.items()), columns=[first_column, "second"])
+            for d in range(0,10):
+                date_top_related_words.iloc[d,0] = f'{date_top_related_words.iloc[d,0]}  :  {date_top_related_words.iloc[d,1]}건'
             date_top_related_words.drop(labels='second', axis=1, inplace=True)
             date_top_related_words.index = date_top_related_words.index + 1
             keywords_table = pd.concat([keywords_table, date_top_related_words], axis=1, join='outer')
+
+    if period % n == 0:
+        pass
+    else:
+        plus_day = start_date + timedelta(days=(n * (i + 1)))
+        str_plus_day = str(plus_day)[5:10].replace('-', '.') + " ~"
+        plus_alpha_day = start_date + timedelta(days=(n * (i + 1)) + period % n -1)
+        str_plus_alpha_day = str(plus_alpha_day)[5:10].replace('-', '.')
+        date_result = result_list[result_list['Date'].between(plus_day, plus_alpha_day)]
+
+        a = set(result_list['Date'])
+        a
+
+        text = '\n'.join(date_result['Body'])
+        lines = text.split(".")
+
+        # 명사 추출
+        words_list = []
+        for line in lines:
+            if search_word in line:
+                nouns = mecab.nouns(line)
+                for noun in nouns:
+                    if len(noun) > 1 and noun not in stop_words and noun != search_word:
+                        words_list.append(noun)  # 한 문장에서 명사들만 추출(일자별)
+                        total_word_list.append(noun)  # 한 문장에서 명사들만 추출(전 기간)
+
+        c = Counter(words_list)
+
+        top_related_words = dict(c.most_common(10))
+
+        if str_plus_day == str_plus_alpha_day:
+            last_column = str_plus_alpha_day
+        else:
+            last_column = f'{str_plus_day}~{str_plus_alpha_day}'
+
+        #date_top_related_words = pd.DataFrame(list(top_related_words.items()),
+        #                                      columns=[str_plus_day, str_plus_alpha_day])
+        #date_top_related_words.index = date_top_related_words.index + 1
+        #keywords_table = pd.concat([keywords_table, date_top_related_words], axis=1, join='outer')
+
+
+
+
+        date_top_related_words = pd.DataFrame(list(top_related_words.items()), columns=[last_column, "second"])
+
+        for i in range(0, 10):
+            date_top_related_words.iloc[i, 0] = f'{date_top_related_words.iloc[i, 0]}  :  {date_top_related_words.iloc[i, 1]}건'
+        date_top_related_words.drop(labels='second', axis=1, inplace=True)
+        date_top_related_words.index = date_top_related_words.index + 1
+        keywords_table = pd.concat([keywords_table, date_top_related_words], axis=1, join='outer')
 
 
 
@@ -326,7 +345,7 @@ if search_word and start_date and end_date != "0":
     top_related_words = dict(c.most_common(50))
     wc = WordCloud(background_color='white', font_path='C:\\github\\project2\\main\\NanumGothic.ttf')
     wc.generate_from_frequencies(top_related_words)
-    figure = plt.figure(figsize= (5, 5))
+    figure = plt.figure(figsize=(width, 5))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
     plt.show()
